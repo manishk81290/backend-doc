45f844a6a829e8cc02ba2bb467ce6c8491a82cd6
Manish Kumar <manishk81290@gmail.com>
2023-08-08 01:02:09 +0530

    changes done in main file

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
-/*!
- * express
- * Copyright(c) 2009-2013 TJ Holowaychuk
- * Copyright(c) 2013 Roman Shtylman
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
- * MIT Licensed
- */
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 
-'use strict';
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-module.exports = require('./lib/express');
+let conversationHistory = '';
+
+app.post('/query', async (req, res) => {
+    const userQuery = req.body.query;
+    conversationHistory += `User: ${userQuery}\n`;
+
+    if (userQuery.toLowerCase().includes('bye doctor')) {
+        conversationHistory = '';
+        return res.json({ response: 'Goodbye! Take care.' });
+    }
+
+    const prompt = `
+    System: You are a medical professional, assistant to a doctor, you only have knowledge about the medical field. You talk to the user until he says goodbye doctor. To any query that is not related to the medical, injuries, sports, nutrition field, I want you to reply with "As a medical assistant, I don't have the answer to this query. Additionally, my main area of expertise is in the medical field.". Just tell the possible medical conditions and suggest remedies or medicines, suggest any scans or tests that the user may take if you feel like so.
+    ${conversationHistory}
+    Assistant:`;
+
+    try {
+        const response = await axios.post(
+            'https://api.openai.com/v1/engines/text-davinci-003/completions',
+            {
+                prompt,
+                temperature: 0.8,
+                max_tokens: 400,
+            },
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer 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',
+                },
+            },
+        );
+
+        const assistantResponse = response.data.choices[0].text.trim();
+        conversationHistory += `Assistant: ${assistantResponse}\n`;
+
+        res.json({ response: assistantResponse });
+    } catch (error) {
+        console.log(error.message+`utkarsh`);
+        res.status(500).json({ error: 'An error occurred while processing the request.' });
+    }
+});
+
+const port = process.env.PORT || 5110;
+app.listen(port, () => {
+    console.log('Server is running on http://localhost:5110');
+});
